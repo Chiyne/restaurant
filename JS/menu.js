748d07b96705bd913e0ebb4ffafbce0828a05492
Chiyne <74550319+Chiyne@users.noreply.github.com>
2021-04-14 10:09:07 +0700

    Add files via upload
    Проект готов для портфолио. Without adaptation for screenreaders

---
+++ JS/menu.js
@@ -0,0 +1,24 @@
+let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.nav__list');
+
+const toggleMenu = () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('nav__list_active');
+}
+
+hamburger.addEventListener('click', e => {
+    //e.stopPropagation();
+    toggleMenu();
+});
+
+document.addEventListener('click', e => {
+    let target = e.target;
+    let its_menu = target == menu;
+    let its_hamburger = target == hamburger;
+    let menu_is_active = menu.classList.contains('nav__list_active');
+
+
+    if (!its_menu && !its_hamburger && menu_is_active) {
+      toggleMenu();
+    }
+});
